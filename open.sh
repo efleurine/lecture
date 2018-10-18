83877e22195884800e0ef6100d149183b5c8c546
Emmanuel <emmanuelfleurine@gmail.com>
2018-10-17 20:27:00 -0400

    chore: Save project progress

---
+++ open.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/eph/Android/Sdk/platform-tools/adb shell input keyevent 82
---
+++ reload.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/home/eph/Android/Sdk/platform-tools/adb shell input keyevent 82 && /home/eph/Android/Sdk/platform-tools/adb shell input keyevent 66 && /home/eph/Android/Sdk/platform-tools/adb shell input keyevent 66
+
